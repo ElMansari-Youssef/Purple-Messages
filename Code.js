3ff6bb7bf0a8c1deddfd1b6ea3b67f8cdaca2df6
ElMansari-Youssef <91879610+ElMansari-Youssef@users.noreply.github.com>
2024-06-25 01:14:59 +0200

    added some js

--- Code.js
+++ Code.js
@@ -1,25 +1,74 @@
-let prsn1 = document.getElementById("prsn1");
-let prsn2 = document.getElementById("prsn2");
-let chatContainer = document.getElementById("chatContainer");
+let chatContainer = document.querySelector(".mainChat .chatContainer");
+let messageBox = document.querySelectorAll(".messageBox");
 let backBtn = document.getElementById("backBtn");
 
 
+messageBox.forEach((element) => {
+
+    element.addEventListener("click", () => {
+
+        chatContainer.style.animation = "chatBox 0.4s ease-in-out forwards";
+
+        id = element.id;
+
+        switch (id) {
+
+            case "contact1":
+
+                setImageSource(element, '.image img');
+
+                break;
+
+            case "contact2":
+
+                setImageSource(element, '.image img');
+
+                break;
+
+            case "contact3":
+
+                setImageSource(element, '.image img');
+
+                break;
+
+            case "contact4":
+
+                setImageSource(element, '.image img');
+
+                break;
+
+            case "contact5":
+
+                setImageSource(element, '.image img');
+
+                break;
 
 
-prsn1.addEventListener("click", function () {
-    
+        }
 
-    chatContainer.style.animation  ="chatBox 0.4s ease-in-out forwards"; 
-    
-    
-    
+
+    })
 })
+
+
 
 backBtn.addEventListener("click", function () {
 
 
-    chatContainer.style.animation  ="chatBox2 0.4s ease-out forwards alternate";
+    chatContainer.style.animation = "chatBox2 0.4s ease-out forwards alternate";
 })
+
+
+
+
+function setImageSource(element, selector) {
+
+    let img = element.querySelector(selector).src;
+    let imageElements = document.querySelectorAll('.chatContainer .message .image img');
+    imageElements[0].src = img;
+    imageElements[3].src = img;
+}
+
 
 
 
